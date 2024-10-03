84dfbc7864148855cf5e4d339e122138540bd5d7
Reppin, Jesse <jesse.reppin@buerokompetenz.de>
2024-10-03 22:36:17 +0200

    edit check output

--- check_zammad.py
+++ check_zammad.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
+
+# Copyright (c) 2024 Marek Beckmann
+# Contributor - hashfunktion - Jesse Reppin
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import requests
 import json
 import sys
 import argparse
-####
-#
-#  Complete Documentation: https://github.com/marekbeckmann/Icinga2-Zammad-Check
-#
-####
 
 # STATES
 OK = 0
@@ -27,7 +44,6 @@ def check(server, token):
     try:
         zammad_data = requests.get(
             server + "/api/v1/monitoring/health_check?token=" + token).text
-
         zammad_data = json.loads(zammad_data)
         status = UNKNOWN
     except:
@@ -40,10 +56,18 @@ def check(server, token):
 
     if health:
         status = OK
+        message['summary'] += 'Zammad healthy'
+        if not issues:
+            message['summary'] += ' - no issues!'
+        if issues:
+            message['summary'] += '\nIssues: \n' + '\n'.join(issues)
+
     else:
         status = CRITICAL
+        message['summary'] += 'Zammad not healthy - see Issues below!'
+        if issues:
+            message['summary'] += '\nIssues:\n' + '\n'.join(issues)
 
-    message['summary'] += '\nIssues: '.join(issues)
     return status
 
 
